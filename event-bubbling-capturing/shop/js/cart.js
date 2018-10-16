d8ac82b2fb8175a7b4c1cf532b3d53bf60a0258b
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-10-17 01:57:29 +0300

    task 3

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const box = document.querySelector('.items-list');
+
+box.addEventListener('click', addCart);
+
+function addCart(evt) {
+  if(!evt.target.classList.contains('add-to-cart')) return;
+  evt.preventDefault();
+
+  const item = {
+    'title': evt.target.dataset.title,
+    'price': evt.target.dataset.price
+  };
+
+  addToCart(item);
+};
