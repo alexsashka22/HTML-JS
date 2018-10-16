f9e5433b54331da15162156cf9c9663bbfaae0cb
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-10-17 01:54:06 +0300

    task 2

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function handleTableClick(event) {
+  let target = event.target.dataset;
+
+  target.dir = +target.dir === 1 ? -1 : 1;
   
-}
+  event.currentTarget.dataset.sortBy = target.propName;
+
+  sortTable(target.propName, +target.dir);
+};
