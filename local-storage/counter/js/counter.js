33c3c15143b1a875bb4638b069069896454e6a5d
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-10-19 22:39:30 +0300

    task 1

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const counter = document.getElementById('counter'),
+      incrementBtn = document.getElementById('increment'),
+      decrementBtn = document.getElementById('decrement'),
+      resetBtn = document.getElementById('reset');
+
+let counterValue;
+
+if (localStorage.counterValue != undefined) {
+  counterValue = localStorage.counterValue;
+  // console.log(localStorage.counterValue);
+} else {
+  counterValue = 0;
+  // console.log(localStorage.counterValue);
+}
+
+
+function resetIndex() {
+  counterValue = 0;
+  // console.log(counterValue);
+  counter.innerHTML = (counterValue);
+  localStorage.counterValue = counterValue;
+}
+
+function incrementIndex() {
+  counterValue++;
+  // console.log(counterValue);
+  counter.innerHTML = (counterValue);
+  localStorage.counterValue = counterValue;
+}
+
+function decrementIndex() {
+  if (counterValue != 0) {
+    counterValue--;
+    // console.log(counterValue);
+    counter.innerHTML = (counterValue);
+    localStorage.counterValue = counterValue;
+  }
+}
+
+counter.innerHTML = (counterValue);
+resetBtn.addEventListener('click', resetIndex);
+incrementBtn.addEventListener('click', incrementIndex);
+decrementBtn.addEventListener('click', decrementIndex);
