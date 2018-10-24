31d7ccca5ed3bca2f9bc47f58e8331ce76db1cb1
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-10-24 13:07:27 +0300

    task 1

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', evt => {
+  showBubbles(evt.currentTarget);
+});
+
+document.addEventListener('click', evt => {
+  const x = evt.clientX;
+  const y = evt.clientY;
+  connection.send(JSON.stringify({
+    x: x,
+    y: y
+  }));
+});
