202fb11672a6446d520fbbfb3717b536ec2baa66
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-11-28 13:16:10 +0300

    task 1 ver 2

--- drag-and-drop/trash-bin/js/script.js
+++ drag-and-drop/trash-bin/js/script.js
@@ -2,37 +2,38 @@
 
 let movedPiece = null;
 
+let ind = false;
+
 document.addEventListener('mousedown', start);
-document.addEventListener('mousemove', drag);
-document.addEventListener('mouseup', drop);
 
 function start(evt) {
-  if (evt.target.classList.contains('logo')) {
+    if (!evt.target.classList.contains('logo')) return;
     movedPiece = evt.target;
-  };
+    movedPiece.classList.add('moving');
+    ind = true;
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', drop);
 };
 
 function drag(evt) {
-  if (movedPiece) {
+    if (!ind) return;
     evt.preventDefault();
     movedPiece.style.left = evt.pageX - movedPiece.offsetWidth / 2 + 'px';
     movedPiece.style.top = evt.pageY - movedPiece.offsetHeight / 2 + 'px';
-    movedPiece.classList.add('moving');
-  };
 };
 
 function drop(evt) {
-  if (movedPiece) {
+    ind = false;
+    movedPiece.classList.remove('moving');
+
     movedPiece.style.visibility = 'hidden';
     const check = document
-      .elementFromPoint(evt.clientX, evt.clientY)
-      .closest('#trash_bin');
+        .elementFromPoint(evt.clientX, evt.clientY)
+        .closest('#trash_bin');
     movedPiece.style.visibility = 'visible';
 
     if (check) {
-      movedPiece.style.display = 'none';
-      movedPiece.classList.remove('moving');
-      movedPiece = null;
+        movedPiece.style.display = 'none';
+        movedPiece = null;
     };
-  };
 };
