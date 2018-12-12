2b19c9afb47615a8e0248ce89d0725e0e6eb9d77
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-12-12 13:45:22 +0300

    task 2

--- media/eye/js/animation.js
+++ media/eye/js/animation.js
@@ -1,1 +1,27 @@
 'use strict';
+const pupilEye = document.querySelector('.big-book__pupil');
+
+document.addEventListener('mousemove', liveEye);
+
+function liveEye(event) {
+  const posX = event.pageX,
+        posY = event.pageY,
+        bodyRect = document.body.getBoundingClientRect(),
+        elemRect = pupilEye.getBoundingClientRect(),
+        offsetX = elemRect.left - bodyRect.left + elemRect.width / 2,
+        offsetY = elemRect.top - bodyRect.top + elemRect.height / 2,
+        deltaX = posX - offsetX,
+        deltaY = posY - offsetY,
+        distance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)),
+        maxDistance = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)),
+        maxSize = 3,
+        minSize = 1,
+        sizePupil = maxSize - (maxSize - minSize) * (distance / maxDistance),
+        maxDelta = 30,
+        pupilDeltaX = maxDelta * (deltaX / offsetX),
+        pupilDeltaY = maxDelta * (deltaY / offsetY);
+
+  pupilEye.style.setProperty('--pupil-size', sizePupil);
+  pupilEye.style.setProperty('--pupil-x', pupilDeltaX + 'px');
+  pupilEye.style.setProperty('--pupil-y', pupilDeltaY + 'px');
+}
