4bbfa11f6575537800a7b8b57ec82ac486368216
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-12-12 13:14:26 +0300

    task 2

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
+window.editor.addEventListener('update', () => {
+  canvas.toBlob(pic => connection.send(pic));
+});
