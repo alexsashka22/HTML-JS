192ae89ee1e3a2b8d06af9ed581a71e50e42d428
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-12-12 13:55:07 +0300

    task 3

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  };
+
+  const el = document.createElement(node.name);
+
+  if (node.props && typeof node.props === 'object') {
+    Object.keys(node.props).forEach(i => el.setAttribute(i, node.props[i]));
+  };
+
+  if (node.childs instanceof Array) {
+    node.childs.forEach(child => el.appendChild(createElement(child)));
+  };
+
+  return el;
+};
