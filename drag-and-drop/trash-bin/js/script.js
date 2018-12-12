72e9d88623ae8d12d0eea94926d31dced5aeb649
Alexandra Smirnova <alexsashka2208@gmail.com>
2018-12-12 13:08:41 +0300

    task 1 ver 4

--- drag-and-drop/trash-bin/js/script.js
+++ drag-and-drop/trash-bin/js/script.js
@@ -4,6 +4,8 @@ let movedPiece = null;
 
 let ind = false;
 
+let isTarget = null;
+
 document.addEventListener('mousedown', start);
 
 function start(evt) {
@@ -17,25 +19,26 @@ function start(evt) {
 
 function drag(evt) {
     if (!ind) return;
+    isTarget = evt.target.id === 'trash_bin' ? evt.target : null;
     evt.preventDefault();
     movedPiece.style.left = evt.pageX - movedPiece.offsetWidth / 2 + 'px';
     movedPiece.style.top = evt.pageY - movedPiece.offsetHeight / 2 + 'px';
 };
 
 function drop(evt) {
-    if (movedPiece) {
-        ind = false;
-        movedPiece.classList.remove('moving');
+  if (movedPiece) {
+    ind = false;
+    movedPiece.classList.remove('moving');
 
-        movedPiece.style.visibility = 'hidden';
-        const check = document
-            .elementFromPoint(evt.clientX, evt.clientY)
-            .closest('#trash_bin');
-        movedPiece.style.visibility = 'visible';
+    movedPiece.style.visibility = 'hidden';
+    const check = document
+        .elementFromPoint(evt.clientX, evt.clientY)
+        .closest('#trash_bin');
+    movedPiece.style.visibility = 'visible';
 
-        if (check) {
-            movedPiece.style.display = 'none';
-            movedPiece = null;
-        };
-    }
+    if (check && isTarget) {
+        movedPiece.style.display = 'none';
+        movedPiece = null;
+    };
+  };
 };
